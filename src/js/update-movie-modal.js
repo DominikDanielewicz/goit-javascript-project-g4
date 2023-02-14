d2794956ee40522d423ad764dcbcf9d4fce07fc7
feiron <pawel.rogowski.praca@gmail.com>
2023-02-14 22:36:10 +0100

    fixed modal button problems + transition properties

--- src/js/update-movie-modal.js
+++ src/js/update-movie-modal.js
@@ -8,53 +8,33 @@ const baseURL = 'http://image.tmdb.org/t/p/w500';
 
 // function generating html from template with movie data
 const fillMovieModalData = (id, movie) => {
-  let getGenres = [...movie.genres].map(genre => genre.name).join(', ');
-  partToFill.innerHTML = `
-  <img class="modal-movie__movie-poster" src="${baseURL + movie.poster_path}" alt="#" />
-  <article class="modal-movie__info">
-    <h2 class="modal-movie__title">${movie.title}</h2>
-    <div class="stats-list__box">
-      <ul class="stats-list">
-        <li>
-          <p>Vote / Votes</p>
-        </li>
-        <li>
-          <p>Popularity</p>
-        </li>
-        <li>
-          <p>Original title</p>
-        </li>
-        <li>
-          <p>Genre</p>
-        </li>
-      </ul>
-      <ul class="stats-list__values">
-        <li>
-          <p><span class="stats-list__highlight">${movie.vote_average}</span> / ${movie.vote_count}</p>
-        </li>
-        <li>
-          <p>${movie.popularity}</p>
-        </li>
-        <li>
-          <p>${movie.original_title}</p>
-        </li>
-        <li>
-          <p>${getGenres}</p>
-        </li>
-      </ul>
-    </div>
-    <div class="movie-description">
-      <h3 class="movie-description__heading">ABOUT</h3>
-      <p class="movie-description__plot">
-        ${movie.overview}
-      </p>
-    </div>
-    <div class="modal-movie__buttons">
-      <button class="modal-movie__button-add-to-watched" data-id="${id}">ADD TO WATCHED</button>
-      <button class="modal-movie__button-add-to-queue" data-id="${id}">ADD TO QUEUE</button>
-    </div>
-  </article>
-  `;
+  // Create a comma-separated list of genres
+  const genres = movie.genres.map(genre => genre.name).join(', ');
+
+  // Get references to all the DOM elements that we want to fill with movie data
+  const modal = document.querySelector('.modal-container');
+  const poster = modal.querySelector('.modal-movie__movie-poster');
+  const title = modal.querySelector('.modal-movie__title');
+  const voteHighlight = modal.querySelector('.stats-list__highlight');
+  const voteTotal = modal.querySelector('.stats-list__vote-total');
+  const popularity = modal.querySelector('.stats-list__popularity-count');
+  const originalTitle = modal.querySelector('.stats-list__original-title');
+  const genreList = modal.querySelector('.stats-list__genres');
+  const description = modal.querySelector('.movie-description__plot');
+  const watchedButton = modal.querySelector('.modal-movie__button-add-to-watched');
+  const queueButton = modal.querySelector('.modal-movie__button-add-to-queue');
+
+  // Fill the DOM elements with movie data
+  poster.src = `${baseURL + movie.poster_path}`;
+  title.textContent = movie.title;
+  voteHighlight.textContent = movie.vote_average;
+  voteTotal.innerHTML = `<span class="stats-list__highlight">${movie.vote_average}</span> / ${movie.vote_count}`;
+  popularity.textContent = movie.popularity;
+  originalTitle.textContent = movie.original_title;
+  genreList.textContent = genres;
+  description.textContent = movie.overview;
+  watchedButton.dataset.id = id;
+  queueButton.dataset.id = id;
 };
 
 function fillData(id) {
