3caa541bfdceb2f8f72bd86b9a21bc96ca2e49a7
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-02-15 19:23:15 +0100

    Watched and Queue buttons implemented

--- src/js/watched.js
+++ src/js/watched.js
@@ -1,20 +1,24 @@
 const watchedBtn = document.querySelector('.modal-movie__button-add-to-watched');
+const queueBtn = document.querySelector('.modal-movie__button-add-to-queue');
 
-let watched = [];
+let watched = JSON.parse(localStorage.getItem('watched')) || [];
+let queue = JSON.parse(localStorage.getItem('queue')) || [];
 
 function addToWatched(event) {
   const button = event.target;
   const movieId = button.dataset.id;
-  watched.push(movieId);
 
-  localStorage.setItem('watched', watched);
-  console.log(`Movie with ID ${movieId} has been added to watched.`);
-  console.log(localStorage.getItem('watched'));
+  watched.push(movieId);
+  localStorage.setItem('watched', JSON.stringify(watched));
+}
+
+function addToQueue(event) {
+  const button = event.target;
+  const movieId = button.dataset.id;
+
+  queue.push(movieId);
+  localStorage.setItem('queue', JSON.stringify(queue));
 }
 
 watchedBtn.addEventListener('click', addToWatched);
-
-const watchedList = localStorage.getItem('watched');
-watchedList.forEach(item => {
-  console.log(item);
-});
+queueBtn.addEventListener('click', addToQueue);
