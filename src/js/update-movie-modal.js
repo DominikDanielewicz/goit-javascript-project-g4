6fb54e3a5afae9a99fcb5ec30baceb02e7d44158
feiron <pawel.rogowski.praca@gmail.com>
2023-02-16 03:28:46 +0100

    modified:   src/js/update-movie-modal.js

--- src/js/update-movie-modal.js
+++ src/js/update-movie-modal.js
@@ -12,20 +12,25 @@ const fillMovieModalData = (id, movie) => {
   const genres = movie.genres.map(genre => genre.name).join(', ');
 
   // Get references to all the DOM elements that we want to fill with movie data
-  const modal = document.querySelector('.modal-container');
-  const poster = modal.querySelector('.modal-movie__movie-poster');
-  const title = modal.querySelector('.modal-movie__title');
-  const voteHighlight = modal.querySelector('.stats-list__highlight');
-  const voteTotal = modal.querySelector('.stats-list__vote-total');
-  const popularity = modal.querySelector('.stats-list__popularity-count');
-  const originalTitle = modal.querySelector('.stats-list__original-title');
-  const genreList = modal.querySelector('.stats-list__genres');
-  const description = modal.querySelector('.movie-description__plot');
-  const watchedButton = modal.querySelector('.modal-movie__button-add-to-watched');
-  const queueButton = modal.querySelector('.modal-movie__button-add-to-queue');
+  const poster = modalElement.querySelector('.modal-movie__movie-poster');
+  const title = modalElement.querySelector('.modal-movie__title');
+  const voteHighlight = modalElement.querySelector('.stats-list__highlight');
+  const voteTotal = modalElement.querySelector('.stats-list__vote-total');
+  const popularity = modalElement.querySelector('.stats-list__popularity-count');
+  const originalTitle = modalElement.querySelector('.stats-list__original-title');
+  const genreList = modalElement.querySelector('.stats-list__genres');
+  const description = modalElement.querySelector('.movie-description__plot');
+  const watchedButton = modalElement.querySelector('.modal-movie__button-add-to-watched');
+  const queueButton = modalElement.querySelector('.modal-movie__button-add-to-queue');
 
   // Fill the DOM elements with movie data
-  poster.src = `${baseURL + movie.poster_path}`;
+  if (movie.poster_path) {
+    poster.src = `${baseURL + movie.poster_path}`;
+  } else {
+    const defaultImageURL =
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png';
+    poster.src = defaultImageURL;
+  }
   title.textContent = movie.title;
   voteHighlight.textContent = movie.vote_average;
   voteTotal.innerHTML = `<span class="stats-list__highlight">${movie.vote_average}</span> / ${movie.vote_count}`;
@@ -37,29 +42,41 @@ const fillMovieModalData = (id, movie) => {
   queueButton.dataset.id = id;
 };
 
-function fillData(id) {
-  fetchMovieById(id).then(movie => {
-    fillMovieModalData(id, movie);
-  });
-}
-gallery.addEventListener('click', event => {
-  let target = event.target;
-  while (target && !target.classList.contains('card')) {
-    target = target.parentElement;
-  }
-  if (target) {
-    const id = target.dataset.id;
-    fillData(id);
-    modalElement.classList.remove('hidden');
-  }
-});
-
-document.addEventListener('keydown', event => {
+function onEscapeKeydown(event) {
   if (event.key === 'Escape') {
     modalElement.classList.add('hidden');
+    document.removeEventListener('keydown', onEscapeKeydown);
+  }
+}
+
+function handleGalleryClick(event) {
+  const target = event.target.closest('.card');
+  if (target) {
+    const id = target.dataset.id;
+    fetchMovieById(id).then(movie => {
+      fillMovieModalData(id, movie);
+    });
+    modalElement.classList.remove('hidden');
+    document.addEventListener('keydown', onEscapeKeydown);
+    gallery.removeEventListener('click', handleGalleryClick);
+  }
+}
+
+gallery.addEventListener('click', event => {
+  const target = event.target.closest('.card');
+  if (target) {
+    const id = target.dataset.id;
+    fetchMovieById(id).then(movie => {
+      fillMovieModalData(id, movie);
+    });
+    modalElement.classList.remove('hidden');
+    document.addEventListener('keydown', onEscapeKeydown);
+    gallery.removeEventListener('click', handleGalleryClick);
   }
 });
 
 modalCloseElement.addEventListener('click', event => {
   modalElement.classList.add('hidden');
+  document.removeEventListener('keydown', onEscapeKeydown);
+  gallery.addEventListener('click', handleGalleryClick);
 });
