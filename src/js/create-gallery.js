8d41582749e94b080bbf9c1c4f666beccfce6c0d
Dominik Danielewicz <d.danielewicz00@gmail.com>
2023-02-18 16:16:49 +0100

    Update create-gallery.js

--- src/js/create-gallery.js
+++ src/js/create-gallery.js
@@ -61,7 +61,7 @@ export async function createGallery(data, key) {
   galleryBox.innerHTML = cards;
 }
 
-if ((PAGINATION_STATE = 'trending')) {
+if (PAGINATION_STATE === 'trending') {
   showSpinner();
   fetchTrending(1).then(data => {
     createGallery(data);
