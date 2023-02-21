3cf17f8222186f69e74a9e948834a92b5d155c16
Dominik Danielewicz <d.danielewicz00@gmail.com>
2023-02-21 13:08:57 +0100

    Updated look and mechanic of
    Modified look and mechanic od authors modal. Change in authors.html, scss and js. Porper authors images uploded.

--- src/js/authors.js
+++ src/js/authors.js
@@ -1,8 +1,93 @@
+import zuzaimage from '../images/zuza.jpg';
+import martaimage from '../images/marta.jpg';
+import dominikimage from '../images/dominik.jpg';
+import pawelimage from '../images/pawel.jpg';
+import dawidimage from '../images/dawid.jpg';
+import rafalimage from '../images/rafal.jpg';
+import patrykimage from '../images/patryk.jpg';
+
+const authors = [
+  {
+    name: 'Zuza Kaźmierczak',
+    photo: zuzaimage,
+    role: 'Developer',
+    description: 'Lorem ipsum Zuza',
+  },
+  {
+    name: 'Marta Dąbrowska',
+    photo: martaimage,
+    role: 'Developer',
+    description: 'Lorem ipsum Marta',
+  },
+  {
+    name: 'Dominik Danielewicz',
+    photo: dominikimage,
+    role: 'Team Lead, Developer',
+    description: 'Lorem ipsum Dominik',
+  },
+  {
+    name: 'Paweł Rogowski',
+    photo: pawelimage,
+    role: 'Manual Tester, Developer',
+    description: 'Lorem ipsum Paweł',
+  },
+  {
+    name: 'Dawid Bartuś',
+    photo: dawidimage,
+    role: 'Scrum Master, Developer',
+    description: 'Lorem ipsum Dawid',
+  },
+  {
+    name: 'Rafał Szewczyk',
+    photo: rafalimage,
+    role: 'Developer',
+    description: 'Lorem ipsum Rafał',
+  },
+  {
+    name: 'Patryk Karolczak',
+    photo: patrykimage,
+    role: 'Developer',
+    description: 'Lorem ipsum Patryk',
+  },
+];
+
 //backdrop and modal window
 const backdrop = document.querySelector('.authors__backdrop');
 //button to open modal window
 const authorButton = document.querySelector('.students-link');
-const modalButton = document.querySelector('.modal__button');
+const modalButton = document.querySelector('.authors__button-close');
+
+const authorsButtons = document.querySelector('.authors__list');
+
+const authorsPhoto = document.querySelector('.authors__photo');
+const authorsName = document.querySelector('.authors__name');
+const authorsRole = document.querySelector('.authors__role');
+const authorsDescription = document.querySelector('.authors__description');
+const authorsTabs = Array.from(authorsButtons.children);
+
+authorsButtons.addEventListener('click', selectAuthor);
+
+function selectAuthor(e) {
+  if (e.target.nodeName !== 'LI') {
+    return;
+  }
+
+  authorsTabs.forEach(tab => {
+    tab.classList.remove('authors__tab--active');
+    tab.classList.add('authors__tab');
+  });
+
+  e.target.classList.add('authors__tab--active');
+
+  for (const { name, photo, role, description } of authors) {
+    if (name.includes(e.target.innerText)) {
+      authorsName.textContent = name;
+      authorsRole.textContent = role;
+      authorsDescription.textContent = description;
+      authorsPhoto.src = photo;
+    }
+  }
+}
 
 const openModal = e => {
   e.preventDefault();
@@ -14,5 +99,4 @@ const closeModal = () => {
 };
 
 authorButton.addEventListener('click', openModal);
-
 modalButton.addEventListener('click', closeModal);
