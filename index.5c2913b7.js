d1c09d82c10ec80a02ad74633be1d464a905b6f7
DominikDanielewicz <d.danielewicz00@gmail.com>
2023-02-21 12:58:43 +0000

    Deploying to gh-pages from @ DominikDanielewicz/goit-javascript-project-g4@6e5c149bcdab56fbd21124a500f2b0ba8840a8ff 🚀

--- index.5c2913b7.js
+++
@@ -1,2 +0,0 @@
-!function(){const e=document.querySelector(".authors__backdrop"),t=document.querySelector(".students-link"),c=document.querySelector(".modal__button");t.addEventListener("click",(t=>{t.preventDefault(),e.classList.replace("hidden","show")})),c.addEventListener("click",(()=>{e.classList.replace("show","hidden")}))}();
-//# sourceMappingURL=index.5c2913b7.js.map
